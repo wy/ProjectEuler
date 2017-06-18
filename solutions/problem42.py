1ff9d2413771c5078814c9b1daaa41794937e66e
wy <wingyungchan@gmail.com>
2017-06-18 15:38:11 +0100

    Solved problem 42 (which was previously in partially solved state). Main challenge with Problem 42 was in trying to be smart about how to generate the triangle numbers. I used a lazy method.

--- solutions/problem42.py
+++ solutions/problem42.py
@@ -5,13 +5,20 @@
 # Coded Triangle Numbers
 # Try and be smart and generate triangle numbers only when needed
 
-
 import string
 di=dict(zip(string.ascii_letters,[ord(c)%32 for c in string.ascii_letters]))
 
+def getscore(word):
+    # Numeric Score based on summing the Alphabet Scores of each letter
+    score = 0
+    for s in word:
+
+        score += di[s]
+    return score
+
 def istriangle(word,triangle_arr):
     score = getscore(word)
-    while score > triangle_arr[-1]
+    while score > triangle_arr[-1]:
         n = 1 + len(triangle_arr)
         triangle_arr.append((n*(n+1))//2)
 
@@ -21,9 +28,19 @@ def istriangle(word,triangle_arr):
 def problem42():
     triangle_arr = [1]
     cnt = 0
+    line = ""
+
     with open('../helperfiles/p042_words.txt','r') as f:
-        for line in f:
-            if istriangle(line,triangle_arr):
-                cnt += 1
-                print(line)
-    
+        line = f.readline()
+
+    words = line.replace('"','').split(",")
+
+    for w in words:
+        if istriangle(w, triangle_arr):
+            cnt += 1
+            print(w)
+
+    return cnt
+
+print(problem42())
+
